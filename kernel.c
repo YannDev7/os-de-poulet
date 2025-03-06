bbf1c89be24b3bb276b25b4dd8f6c420490faf96
ordiasuszenbook <leberre.amedee@gmail.com>
2025-03-06 16:57:22 +0100

    dsfsdfsd

--- kernel.c
+++ kernel.c
@@ -92,15 +92,23 @@ void terminal_putchar(char c)
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
+		if (++terminal_row == VGA_HEIGHT) {
 			terminal_row = 0;
+
+		}
 	}
 }
 
 void terminal_write(const char* data, size_t size) 
 {
 	for (size_t i = 0; i < size; i++)
-		terminal_putchar(data[i]);
+		if (data[i] == '\n') {
+			terminal_row += 1;
+			terminal_column = 0;
+		} else {
+			terminal_putchar(data[i]);
+
+		}
 }
 
 void terminal_writestring(const char* data) 
@@ -114,5 +122,12 @@ void kernel_main(void)
 	terminal_initialize();
 
 	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\n");
+	int i = 0;
+
+	for (int i = 0; i < 2; ++i) {
+		terminal_writestring("Hello, kernel World!\nBloup");
+
+	}
+
+	
 }
